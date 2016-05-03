15a67bbda88997e30974e207fe2fe6d44d1922f8
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2016-05-03 17:46:15 +0200

    Add comment to remember to change TTL.

--- include/partisan.hrl
+++ include/partisan.hrl
@@ -4,6 +4,8 @@
 -define(PEER_PORT, 9000).
 -define(PEER_SERVICE_SERVER, partisan_peer_service_server).
 -define(FANOUT, 5).
+
+%% @todo Reduce me.
 -define(GOSSIP_INTERVAL, 300).
 
 -type node_spec() :: {node(), inet:ip_address(), non_neg_integer()}.
