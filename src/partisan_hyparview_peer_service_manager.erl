b319e69137a428314eda7d2127c195cfb0669197
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2016-06-29 15:58:47 +0200

    Remove excessive logging.

--- src/partisan_hyparview_peer_service_manager.erl
+++ src/partisan_hyparview_peer_service_manager.erl
@@ -717,13 +717,11 @@ do_send_message(Name, Message, Connections) when is_atom(Name) ->
     case dict:find(Name, Connections) of
         {ok, undefined} ->
             %% Node was connected but is now disconnected.
-            lager:error("Node ~p disconnected.", [Name]),
             {error, disconnected};
         {ok, Pid} ->
             gen_server:cast(Pid, {send_message, Message});
         error ->
             %% Node has not been connected yet.
-            lager:error("Node ~p not yet connected.", [Name]),
             {error, not_yet_connected}
     end;
 do_send_message({Name, _, _}, Message, Connections) ->
