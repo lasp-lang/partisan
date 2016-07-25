7d00f781da3066be90d31f3ed2c1c19aa759494f
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2016-07-25 10:48:27 +0200

    Add test for default peer service manager.

---
+++ test/partisan_SUITE.erl
@@ -0,0 +1,235 @@
+%% -------------------------------------------------------------------
+%%
+%% Copyright (c) 2016 Christopher Meiklejohn.  All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+%%
+
+-module(partisan_SUITE).
+-author("Christopher Meiklejohn <christopher.meiklejohn@gmail.com>").
+
+%% common_test callbacks
+-export([%% suite/0,
+         init_per_suite/1,
+         end_per_suite/1,
+         init_per_testcase/2,
+         end_per_testcase/2,
+         all/0]).
+
+%% tests
+-compile([export_all]).
+
+-include_lib("common_test/include/ct.hrl").
+-include_lib("eunit/include/eunit.hrl").
+-include_lib("kernel/include/inet.hrl").
+
+-define(APP, partisan).
+-define(CT_SLAVES, [rita, sue, bob, jerome]).
+-define(PEER_PORT, 9000).
+
+%% ===================================================================
+%% common_test callbacks
+%% ===================================================================
+
+init_per_suite(_Config) ->
+    _Config.
+
+end_per_suite(_Config) ->
+    _Config.
+
+init_per_testcase(Case, _Config) ->
+    ct:pal("Beginning test case ~p", [Case]),
+
+    _Config.
+
+end_per_testcase(Case, _Config) ->
+    ct:pal("Ending test case ~p", [Case]),
+
+    _Config.
+
+all() ->
+    [
+     default_manager_test
+    ].
+
+%% ===================================================================
+%% Tests.
+%% ===================================================================
+
+default_manager_test(Config) ->
+    %% Start nodes.
+    Nodes = start(default_manager_test, Config, []),
+
+    %% Use the default peer service manager.
+    Manager = partisan_default_peer_service_manager,
+
+    %% Pause for clustering.
+    timer:sleep(1000),
+
+    %% Verify membership.
+    %%
+    %% Every node should know about every other node in this topology.
+    %%
+    VerifyFun = fun(Node) ->
+            {ok, Members} = rpc:call(Node, Manager, members, []),
+            SortedNodes = lists:usort(Nodes),
+            SortedMembers = lists:usort(Members),
+            case SortedMembers =:= SortedNodes of
+                true ->
+                    ok;
+                false ->
+                    ct:fail("Membership incorrect; node ~p should have ~p but has ~p", [Node, Nodes, Members])
+            end
+    end,
+
+    %% Verify the membership is correct.
+    lists:foreach(VerifyFun, Nodes),
+
+    %% Stop nodes.
+    stop(Nodes),
+
+    ok.
+
+%% ===================================================================
+%% Internal functions.
+%% ===================================================================
+
+%% @private
+start(_Case, _Config, Options) ->
+    %% Launch distribution for the test runner.
+    ct:pal("Launching Erlang distribution..."),
+
+    os:cmd(os:find_executable("epmd") ++ " -daemon"),
+    {ok, Hostname} = inet:gethostname(),
+    case net_kernel:start([list_to_atom("runner@" ++ Hostname), shortnames]) of
+        {ok, _} ->
+            ok;
+        {error, {already_started, _}} ->
+            ok
+    end,
+
+    %% Load sasl.
+    application:load(sasl),
+    ok = application:set_env(sasl,
+                             sasl_error_logger,
+                             false),
+    application:start(sasl),
+
+    %% Load lager.
+    {ok, _} = application:ensure_all_started(lager),
+
+    %% Start all three nodes.
+    InitializerFun = fun(Name) ->
+                            ct:pal("Starting node: ~p", [Name]),
+
+                            NodeConfig = [{monitor_master, true},
+                                          {startup_functions, [{code, set_path, [codepath()]}]}],
+
+                            case ct_slave:start(Name, NodeConfig) of
+                                {ok, Node} ->
+                                    Node;
+                                Error ->
+                                    ct:fail(Error)
+                            end
+                     end,
+    [_First|_] = Nodes = lists:map(InitializerFun, ?CT_SLAVES),
+
+    %% Load applications on all of the nodes.
+    LoaderFun = fun(Node) ->
+                            ct:pal("Loading applications on node: ~p", [Node]),
+
+                            PrivDir = code:priv_dir(?APP),
+                            NodeDir = filename:join([PrivDir, "lager", Node]),
+
+                            %% Manually force sasl loading, and disable the logger.
+                            ok = rpc:call(Node, application, load, [sasl]),
+                            ok = rpc:call(Node, application, set_env,
+                                          [sasl, sasl_error_logger, false]),
+                            ok = rpc:call(Node, application, start, [sasl]),
+
+                            ok = rpc:call(Node, application, load, [partisan]),
+                            ok = rpc:call(Node, application, load, [lager]),
+                            ok = rpc:call(Node, application, set_env, [sasl,
+                                                                       sasl_error_logger,
+                                                                       false]),
+                            ok = rpc:call(Node, application, set_env, [lager,
+                                                                       log_root,
+                                                                       NodeDir])
+                     end,
+    lists:map(LoaderFun, Nodes),
+
+    %% Configure settings.
+    ConfigureFun = fun(Node) ->
+                        %% Configure the peer service.
+                        PeerService = proplists:get_value(partisan_peer_service_manager, Options),
+                        ok = rpc:call(Node, partisan_config, set,
+                                      [partisan_peer_service_manager, PeerService])
+                   end,
+    lists:map(ConfigureFun, Nodes),
+
+    ct:pal("Starting nodes."),
+
+    StartFun = fun(Node) ->
+                        %% Start partisan.
+                        {ok, _} = rpc:call(Node, application, ensure_all_started, [partisan])
+                   end,
+    lists:map(StartFun, Nodes),
+
+    ct:pal("Custering nodes..."),
+    lists:map(fun(Node) -> cluster(Node, Nodes) end, Nodes),
+
+    ct:pal("Partisan fully initialized."),
+
+    Nodes.
+
+%% @private
+codepath() ->
+    lists:filter(fun filelib:is_dir/1, code:get_path()).
+
+%% @private
+%%
+%% We have to cluster each node with all other nodes to compute the
+%% correct overlay: for instance, sometimes you'll want to establish a
+%% client/server topology, which requires all nodes talk to every other
+%% node to correctly compute the overlay.
+%%
+cluster(Node, Nodes) when is_list(Nodes) ->
+    lists:map(fun(OtherNode) -> cluster(Node, OtherNode) end, Nodes -- [Node]);
+cluster(Node, OtherNode) ->
+    PeerPort = rpc:call(OtherNode,
+                        partisan_config,
+                        get,
+                        [peer_port, ?PEER_PORT]),
+    ct:pal("Joining node: ~p to ~p at port ~p", [Node, OtherNode, PeerPort]),
+    ok = rpc:call(Node,
+                  partisan_peer_service,
+                  join,
+                  [{OtherNode, {127, 0, 0, 1}, PeerPort}]).
+
+%% @private
+stop(_Nodes) ->
+    StopFun = fun(Node) ->
+        case ct_slave:stop(Node) of
+            {ok, _} ->
+                ok;
+            Error ->
+                ct:fail(Error)
+        end
+    end,
+    lists:map(StopFun, ?CT_SLAVES),
+    ok.
+
