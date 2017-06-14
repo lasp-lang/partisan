4ed2946dd842ddff73c469f06365d5d279ef3ece
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2017-06-13 18:40:07 -0700

    Attempt to use the cache to fast-path forwarding requests.

--- src/partisan_default_peer_service_manager.erl
+++ src/partisan_default_peer_service_manager.erl
@@ -105,7 +105,15 @@ send_message(Name, Message) ->
 
 %% @doc Forward message to registered process on the remote side.
 forward_message(Name, ServerRef, Message) ->
-    gen_server:call(?MODULE, {forward_message, Name, ServerRef, Message}, infinity).
+    FullMessage = {forward_message, Name, ServerRef, Message},
+
+    %% Attempt to fast-path through the memoized connection cache.
+    case partisan_connection_cache:dispatch(FullMessage) of
+        ok ->
+            ok;
+        {error, trap} ->
+            gen_server:call(?MODULE, FullMessage, infinity)
+    end.
 
 %% @doc Receive message from a remote manager.
 receive_message(Message) ->
@@ -470,6 +478,9 @@ establish_connections(Pending,
                                          {Connections0, MemberParallelism},
                                          without_me(Peers)),
 
+    %% Memoize connections.
+    partisan_connection_cache:update(Connections),
+
     %% Return the updated list of connections.
     Connections.
 
@@ -503,8 +514,6 @@ maybe_connect({Name, _, _} = Node, {Connections0, MemberParallelism}) ->
             end;
         %% Found in dict and connected.
         {ok, Pids} ->
-            lager:info("Node ~p connected with ~p processes of ~p processes.",
-                       [Node, length(Pids), Parallelism]),
             case length(Pids) < Parallelism of
                 true ->
                     lager:info("Connecting node ~p.", [Node]),
