4b066552efc355a034ce7c2f57acbfd64765c259
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2017-08-05 16:10:22 -0700

    Store just the node name in the cache.

--- src/partisan_connection_cache.erl
+++ src/partisan_connection_cache.erl
@@ -31,8 +31,8 @@
 update(Connections) ->
     ets:delete_all_objects(?CACHE),
 
-    dict:fold(fun(K, V, _AccIn) ->
-                      true = ets:insert(?CACHE, [{K, V}])
+    dict:fold(fun(#{name := Name}, V, _AccIn) ->
+                      true = ets:insert(?CACHE, [{Name, V}])
               end, [], Connections).
 
 dispatch({forward_message, Name, ServerRef, Message}) ->
