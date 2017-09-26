bbe39da020a5f29b1b8793d93291417f9dc32af0
Tristan Sloughter <t@crashfast.com>
2017-09-26 11:13:05 -0700

    remove use of IP and PEER_PORT os vars

--- include/partisan.hrl
+++ include/partisan.hrl
@@ -1,7 +1,6 @@
 -define(APP, partisan).
 -define(PEER_IP, {127, 0, 0, 1}).
 -define(PEER_PORT, 9000).
--define(LISTEN_ADDRS, [#{ip => ?PEER_IP, port => ?PEER_PORT}]).
 -define(PEER_SERVICE_SERVER, partisan_peer_service_server).
 -define(FANOUT, 5).
 -define(CACHE, partisan_connection_cache).
