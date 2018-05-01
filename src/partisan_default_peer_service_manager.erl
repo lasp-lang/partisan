0b6d0b317c53b3ff39caa82d6d70130c7f3ef168
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2018-05-01 13:04:42 -0300

    Don't tag messages for acknowledgements when not necessary.

--- src/partisan_default_peer_service_manager.erl
+++ src/partisan_default_peer_service_manager.erl
@@ -465,17 +465,23 @@ handle_call({forward_message, Name, Channel, Clock, PartitionKey, ServerRef, Mes
     %% Store for reliability, if necessary.
     case proplists:get_value(ack, Options, false) of
         false ->
+            %% Send message along.
+            Result = do_send_message(Name,
+                                    Channel,
+                                    PartitionKey,
+                                    {forward_message, ServerRef, Message},
+                                    Connections),
             ok;
         true ->
+            %% Send message along.
+            Result = do_send_message(Name,
+                                    Channel,
+                                    PartitionKey,
+                                    {forward_message, myself(), MessageClock, ServerRef, Message},
+                                    Connections),
+
             partisan_reliability_backend:store(MessageClock, FullMessage)
     end,
-
-    %% Send message along.
-    Result = do_send_message(Name,
-                             Channel,
-                             PartitionKey,
-                             {forward_message, myself(), MessageClock, ServerRef, Message},
-                             Connections),
 
     {reply, Result, State#state{vclock=VClock}};
 
