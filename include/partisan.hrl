e6774b2ac3a483cc42603a943d843fda3ad69deb
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2018-06-11 13:38:39 -0700

    Change default communication port.

--- include/partisan.hrl
+++ include/partisan.hrl
@@ -1,6 +1,6 @@
 -define(APP, partisan).
 -define(PEER_IP, {127, 0, 0, 1}).
--define(PEER_PORT, 9000).
+-define(PEER_PORT, 9090).
 -define(PEER_SERVICE_SERVER, partisan_peer_service_server).
 -define(FANOUT, 5).
 -define(CACHE, partisan_connection_cache).
