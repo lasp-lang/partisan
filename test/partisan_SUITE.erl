afa6f0bfead1b3d2fa162a2b74a3e352deb74c93
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2018-09-22 22:35:13 -0400

    Remove empty group containing hyparview tests.

--- test/partisan_SUITE.erl
+++ test/partisan_SUITE.erl
@@ -150,7 +150,7 @@ all() ->
 
      {group, with_partition_key, [parallel]},
 
-     {group, with_broadcast, [parallel]},
+     %% {group, with_broadcast, [parallel]},
 
      {group, with_ingress_delay, [parallel]},
 
@@ -244,13 +244,14 @@ groups() ->
       [default_manager_test]},
 
      {with_egress_delay, [],
-      [default_manager_test]},
+      [default_manager_test]}
 
-     {with_broadcast, [],
-      [
-       % hyparview_manager_low_active_test,
-       % hyparview_manager_high_active_test
-      ]}
+     %% TODO: Re-enable me.
+     %% {with_broadcast, [],
+     %%  [
+     %%   hyparview_manager_low_active_test,
+     %%   hyparview_manager_high_active_test
+     %%  ]}
 
     ].
 
