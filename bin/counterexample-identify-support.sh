0d8d9a9f7c23c4e58cd9e1cc9387e89aa3d867a5
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2019-02-19 18:45:35 -0500

    Supply a preload omission file.

--- bin/counterexample-identify-support.sh
+++ bin/counterexample-identify-support.sh
@@ -5,6 +5,7 @@ REPLAY_TRACE_FILE=/tmp/partisan-replay.trace
 SAVED_TRACE_FILE=/tmp/partisan-saved.trace
 COUNTEREXAMPLE_CONSULT_FILE=/tmp/partisan-counterexample.consult
 REBAR_COUNTEREXAMPLE_CONSULT_FILE=_build/test/rebar3_proper-counterexamples.consult
+PRELOAD_OMISSION_FILE=/tmp/partisan-preload.trace
 
 if [ ! -f ${COUNTEREXAMPLE_CONSULT_FILE} ]; then
     echo "No counterexample consult file!"
@@ -21,7 +22,7 @@ cp ${TRACE_FILE} ${SAVED_TRACE_FILE}
 
 # Shrink counterexample, which should copy files in place.
 echo "Deriving schedules to test for counterexample support..."
-bin/counterexample-identify-support.escript ${TRACE_FILE} ${REPLAY_TRACE_FILE} ${COUNTEREXAMPLE_CONSULT_FILE} ${REBAR_COUNTEREXAMPLE_CONSULT_FILE}
+bin/counterexample-identify-support.escript ${TRACE_FILE} ${REPLAY_TRACE_FILE} ${COUNTEREXAMPLE_CONSULT_FILE} ${REBAR_COUNTEREXAMPLE_CONSULT_FILE} ${PRELOAD_OMISSION_FILE}
 
 # # Replay counterexample.
 # echo "Replaying counterexample..."
