baa028f24745a01456778a252a939ca4d8896c22
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2019-02-26 21:06:16 -0500

    Add sublist parameter.

--- bin/wat-provenance.sh
+++ bin/wat-provenance.sh
@@ -4,5 +4,4 @@ echo "Running example suite to identify minimal successful example..."
 rm -rf priv/lager; pkill -9 beam.smp; NUM_TESTS=3 SCHEDULER=single_success bin/counterexample-find.sh
 
 echo "Beginning reduction to find support for successful example..."
-bin/counterexample-identify-support.sh
-
+SUBLIST=${SUBLIST} IMPLEMENTATION_MODULE=lampson_2pc bin/counterexample-identify-support.sh
