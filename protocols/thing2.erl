89a6242e5b68d6bd16f8224d6560bbcd307f83ae
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2019-03-13 13:55:15 -0400

    Fix compilation errors.

--- protocols/thing2.erl
+++ protocols/thing2.erl
@@ -1,6 +1,6 @@
 -module(thing2).
 -export([handle_info/2]).
-handle_info({message1, _A}, State) ->
+handle_info({message1, _A}, _State) ->
     ok;
-handle_info({message2, _A}, State) ->
+handle_info({message2, _A}, _State) ->
     ok.
