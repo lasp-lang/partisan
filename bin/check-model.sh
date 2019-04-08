6a801ec22de55c415c01c59989a23332c780e004
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2019-04-08 09:20:23 -0400

    Add optional noise parameter.

--- bin/check-model.sh
+++ bin/check-model.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 echo "Running example suite to identify minimal successful example..."
-rm -rf priv/lager; pkill -9 beam.smp; IMPLEMENTATION_MODULE=${MODULE} NUM_TESTS=3 SCHEDULER=single_success bin/counterexample-find.sh
+rm -rf priv/lager; pkill -9 beam.smp; NOISE=${NOISE} IMPLEMENTATION_MODULE=${MODULE} NUM_TESTS=3 SCHEDULER=single_success bin/counterexample-find.sh
 
 echo "Performing static analaysis..."
 IMPLEMENTATION_MODULE=${MODULE} bin/partisan-analysis.escript protocols/$MODULE
 
 echo "Beginning reduction to find support for successful example..."
-PRELOAD_SCHEDULES=${PRELOAD_SCHEDULES} RECURSIVE=${RECURSIVE} SUBLIST=${SUBLIST} EXIT_ON_COUNTEREXAMPLE=${EXIT_ON_COUNTEREXAMPLE} IMPLEMENTATION_MODULE=${MODULE} bin/counterexample-identify-support.sh
+NOISE=${NOISE} PRELOAD_SCHEDULES=${PRELOAD_SCHEDULES} RECURSIVE=${RECURSIVE} SUBLIST=${SUBLIST} EXIT_ON_COUNTEREXAMPLE=${EXIT_ON_COUNTEREXAMPLE} IMPLEMENTATION_MODULE=${MODULE} bin/counterexample-identify-support.sh
