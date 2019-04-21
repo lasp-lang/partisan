a68fdce8bd00572e32f0771639f2cde999aaeb73
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2019-04-20 21:05:29 -0400

    Comment out analysis and reduction.

--- bin/check-model.sh
+++ bin/check-model.sh
@@ -3,8 +3,8 @@
 echo "Running example suite to identify minimal successful example..."
 rm -rf priv/lager; pkill -9 beam.smp; NOISE=${NOISE} IMPLEMENTATION_MODULE=${MODULE} NUM_TESTS=3 SCHEDULER=single_success bin/counterexample-find.sh
 
-echo "Performing static analaysis..."
-IMPLEMENTATION_MODULE=${MODULE} bin/partisan-analysis.escript protocols/$MODULE
+# echo "Performing static analaysis..."
+# IMPLEMENTATION_MODULE=${MODULE} bin/partisan-analysis.escript protocols/$MODULE
 
-echo "Beginning reduction to find support for successful example..."
-NOISE=${NOISE} PRELOAD_SCHEDULES=${PRELOAD_SCHEDULES} RECURSIVE=${RECURSIVE} SUBLIST=${SUBLIST} EXIT_ON_COUNTEREXAMPLE=${EXIT_ON_COUNTEREXAMPLE} IMPLEMENTATION_MODULE=${MODULE} bin/counterexample-identify-support.sh
+# echo "Beginning reduction to find support for successful example..."
+# NOISE=${NOISE} PRELOAD_SCHEDULES=${PRELOAD_SCHEDULES} RECURSIVE=${RECURSIVE} SUBLIST=${SUBLIST} EXIT_ON_COUNTEREXAMPLE=${EXIT_ON_COUNTEREXAMPLE} IMPLEMENTATION_MODULE=${MODULE} bin/counterexample-identify-support.sh
