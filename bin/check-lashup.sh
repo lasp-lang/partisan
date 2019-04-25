98d622cec9c6f5a98b9f0eb4586ea50450f56ee2
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2019-04-25 18:46:39 -0400

    Flesh out more of lashup.

--- bin/check-lashup.sh
+++ bin/check-lashup.sh
@@ -9,6 +9,9 @@ export RESTART_NODES=true
 # echo "Running example suite to identify minimal successful example..."
 # rm -rf priv/lager; pkill -9 beam.smp; SYSTEM_MODEL=${SYSTEM_MODEL} RESTART_NODES=${RESTART_NODES} NOISE=${NOISE} IMPLEMENTATION_MODULE=${MODULE} NUM_TESTS=3 SCHEDULER=single_success bin/counterexample-find.sh
 
+# echo "Running single-run execution..."
+# rm -rf priv/lager; pkill -9 beam.smp; SYSTEM_MODEL=${SYSTEM_MODEL} RESTART_NODES=${RESTART_NODES} NOISE=${NOISE} IMPLEMENTATION_MODULE=${MODULE} SCHEDULER=single_success bin/counterexample-find.sh
+
 # echo "Beginning reduction to find support for successful example..."
 # ERL_LIBS=_build/default/lib SYSTEM_MODEL=${SYSTEM_MODEL} USE_STARTED_NODES=${USE_STARTED_NODES} RESTART_NODES=${RESTART_NODES} NOISE=${NOISE} PRELOAD_SCHEDULES=${PRELOAD_SCHEDULES} RECURSIVE=${RECURSIVE} SUBLIST=${SUBLIST} EXIT_ON_COUNTEREXAMPLE=${EXIT_ON_COUNTEREXAMPLE} IMPLEMENTATION_MODULE=${MODULE} bin/counterexample-model-checker.sh
 
