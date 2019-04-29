3518359ba1f13168f01a8fe66d67b6e361dd1b76
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2019-04-29 17:22:27 -0400

    Reenable static analysis.

--- bin/check-paxoid.sh
+++ bin/check-paxoid.sh
@@ -8,6 +8,9 @@ export EXIT_ON_COUNTEREXAMPLE=true
 export PRUNING=false
 export SYSTEM_MODEL=prop_partisan_paxoid
 
+echo "Performing static analaysis..."
+IMPLEMENTATION_MODULE=${MODULE} bin/partisan-analysis.escript _checkouts/paxoid/src/paxoid.erl
+
 echo "Running example suite to identify minimal successful example..."
 rm -rf priv/lager; pkill -9 beam.smp; DISABLE_RANDOM=true RESTART_NODES=false IMPLEMENTATION_MODULE=${MODULE} NUM_TESTS=3 SCHEDULER=single_success bin/counterexample-find.sh
 
