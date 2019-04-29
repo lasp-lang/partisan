9ed0bd0b9201060aeec3130a03deccb1dd63db4a
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2019-04-29 08:02:22 -0400

    Alter test scripts.

--- bin/check-lashup.sh
+++ bin/check-lashup.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-export USE_STARTED_NODES=false
-export RESTART_NODES=true
+export RESTART_NODES=false
+export SYSTEM_MODEL=prop_partisan_lashup
 
 # echo "Performing static analaysis..."
 # IMPLEMENTATION_MODULE=${MODULE} bin/partisan-analysis.escript protocols/$MODULE
 
-# echo "Running example suite to identify minimal successful example..."
-# rm -rf priv/lager; pkill -9 beam.smp; SYSTEM_MODEL=${SYSTEM_MODEL} RESTART_NODES=${RESTART_NODES} NOISE=${NOISE} IMPLEMENTATION_MODULE=${MODULE} NUM_TESTS=3 SCHEDULER=single_success bin/counterexample-find.sh
+echo "Running example suite to identify minimal successful example..."
+rm -rf priv/lager; pkill -9 beam.smp; SYSTEM_MODEL=${SYSTEM_MODEL} RESTART_NODES=${RESTART_NODES} NOISE=${NOISE} IMPLEMENTATION_MODULE=${MODULE} NUM_TESTS=3 SCHEDULER=single_success bin/counterexample-find.sh
 
 # echo "Running single-run execution..."
 # rm -rf priv/lager; pkill -9 beam.smp; SYSTEM_MODEL=${SYSTEM_MODEL} RESTART_NODES=${RESTART_NODES} NOISE=${NOISE} IMPLEMENTATION_MODULE=${MODULE} SCHEDULER=single_success bin/counterexample-find.sh
@@ -15,5 +15,5 @@ export RESTART_NODES=true
 # echo "Beginning reduction to find support for successful example..."
 # ERL_LIBS=_build/default/lib SYSTEM_MODEL=${SYSTEM_MODEL} USE_STARTED_NODES=${USE_STARTED_NODES} RESTART_NODES=${RESTART_NODES} NOISE=${NOISE} PRELOAD_SCHEDULES=${PRELOAD_SCHEDULES} RECURSIVE=${RECURSIVE} SUBLIST=${SUBLIST} EXIT_ON_COUNTEREXAMPLE=${EXIT_ON_COUNTEREXAMPLE} IMPLEMENTATION_MODULE=${MODULE} bin/counterexample-model-checker.sh
 
-echo "Running multi-run execution..."
-rm -rf priv/lager; pkill -9 beam.smp; SYSTEM_MODEL=${SYSTEM_MODEL} RESTART_NODES=${RESTART_NODES} NOISE=${NOISE} IMPLEMENTATION_MODULE=${MODULE} FAULT_INJECTION=true SCHEDULER=finite_fault bin/counterexample-find.sh
+# echo "Running multi-run execution..."
+# rm -rf priv/lager; pkill -9 beam.smp; SYSTEM_MODEL=${SYSTEM_MODEL} RESTART_NODES=${RESTART_NODES} NOISE=${NOISE} IMPLEMENTATION_MODULE=${MODULE} FAULT_INJECTION=true SCHEDULER=finite_fault bin/counterexample-find.sh
--- bin/check-paxoid.sh
+++ bin/check-paxoid.sh
@@ -6,9 +6,13 @@ export PRELOAD_SCHEDULES=false
 export RECURSIVE=true
 export EXIT_ON_COUNTEREXAMPLE=true
 export PRUNING=false
+export SYSTEM_MODEL=prop_partisan_paxoid
 
 echo "Running example suite to identify minimal successful example..."
-rm -rf priv/lager; pkill -9 beam.smp; IMPLEMENTATION_MODULE=${MODULE} NUM_TESTS=3 SCHEDULER=single_success bin/counterexample-find.sh
+rm -rf priv/lager; pkill -9 beam.smp; DISABLE_RANDOM=true RESTART_NODES=false IMPLEMENTATION_MODULE=${MODULE} NUM_TESTS=3 SCHEDULER=single_success bin/counterexample-find.sh
+
+echo "Attemping single run execution..."
+pkill -9 beam.smp; TRACE_FILE=/tmp/partisan-latest.trace REPLAY_TRACE_FILE=/tmp/partisan-replay.trace PRELOAD_OMISSIONS_FILE=/tmp/partisan-preload.trace SHRINKING=true REPLAY=true SYSTEM_MODEL=${SYSTEM_MODEL} IMPLEMENTATION_MODULE=${MODULE} RESTART_NODES=false ./rebar3 ct --readable=false -v --suite=filibuster_SUITE
 
 # echo "Attempting single replay."
 # IMPLEMENTATION_MODULE=${MODULE} bin/counterexample-replay.sh
