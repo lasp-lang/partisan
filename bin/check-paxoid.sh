79c6edc8162a7718c27aa6ce31116f6b00374b1f
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2019-04-29 22:37:39 -0400

    Ensure random usage is disabled.

--- bin/check-paxoid.sh
+++ bin/check-paxoid.sh
@@ -15,7 +15,7 @@ echo "Running example suite to identify minimal successful example..."
 rm -rf priv/lager; pkill -9 beam.smp; DISABLE_RANDOM=true RESTART_NODES=false IMPLEMENTATION_MODULE=${MODULE} NUM_TESTS=3 SCHEDULER=single_success bin/counterexample-find.sh
 
 echo "Attemping single run execution..."
-pkill -9 beam.smp; TRACE_FILE=/tmp/partisan-latest.trace REPLAY_TRACE_FILE=/tmp/partisan-replay.trace PRELOAD_OMISSIONS_FILE=/tmp/partisan-preload.trace SHRINKING=true REPLAY=true SYSTEM_MODEL=${SYSTEM_MODEL} IMPLEMENTATION_MODULE=${MODULE} RESTART_NODES=false ./rebar3 ct --readable=false -v --suite=filibuster_SUITE
+pkill -9 beam.smp; DISABLE_RANDOM=true TRACE_FILE=/tmp/partisan-latest.trace REPLAY_TRACE_FILE=/tmp/partisan-replay.trace PRELOAD_OMISSIONS_FILE=/tmp/partisan-preload.trace SHRINKING=true REPLAY=true SYSTEM_MODEL=${SYSTEM_MODEL} IMPLEMENTATION_MODULE=${MODULE} RESTART_NODES=false ./rebar3 ct --readable=false -v --suite=filibuster_SUITE
 
 # echo "Attempting single replay."
 # IMPLEMENTATION_MODULE=${MODULE} bin/counterexample-replay.sh
