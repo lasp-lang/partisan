655a2cfcd2796f6335ac0d2241bf000f60a2d852
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2019-05-06 18:23:07 -0400

    Reduce number of tests.

--- bin/check-zraft.sh
+++ bin/check-zraft.sh
@@ -24,4 +24,4 @@ export SYSTEM_MODEL=prop_partisan_zraft
 # rm -rf priv/lager; pkill -9 beam.smp; DISABLE_RANDOM=true RESTART_NODES=false IMPLEMENTATION_MODULE=${MODULE} NUM_TESTS=10 SCHEDULER=finite_fault bin/counterexample-find.sh
 
 echo "Running multi run fault-injector..."
-rm -rf priv/lager; pkill -9 beam.smp; DISABLE_RANDOM=true RESTART_NODES=false IMPLEMENTATION_MODULE=${MODULE} NUM_TESTS=100 SCHEDULER=finite_fault FAULT_INJECTION=true bin/counterexample-find.sh
+rm -rf priv/lager; pkill -9 beam.smp; DISABLE_RANDOM=true RESTART_NODES=false IMPLEMENTATION_MODULE=${MODULE} NUM_TESTS=10 SCHEDULER=finite_fault FAULT_INJECTION=true bin/counterexample-find.sh
