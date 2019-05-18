e1b937ed96ae92cc33c48a358727db812c2bf52c
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2019-05-18 18:29:33 -0400

    switch to f=1

--- bin/check-hbbft.sh
+++ bin/check-hbbft.sh
@@ -15,11 +15,11 @@ export PARTISAN=true
 # echo "Running example suite to identify minimal successful example [without bootstrap]..."
 # rm -rf priv/lager; pkill -9 beam.smp; RESTART_NODES=false IMPLEMENTATION_MODULE=${MODULE} NUM_TESTS=3 SCHEDULER=single_success bin/counterexample-find.sh
 
-echo "Running multi run fault-injector [without fault-injection]..."
-rm -rf priv/lager; pkill -9 beam.smp; RESTART_NODES=false IMPLEMENTATION_MODULE=${MODULE} NUM_TESTS=10 SCHEDULER=finite_fault bin/counterexample-find.sh
+# echo "Running multi run fault-injector [without fault-injection]..."
+# rm -rf priv/lager; pkill -9 beam.smp; RESTART_NODES=false IMPLEMENTATION_MODULE=${MODULE} NUM_TESTS=10 SCHEDULER=finite_fault bin/counterexample-find.sh
 
-# echo "Running multi run fault-injector [with fault-injection, F = 1]..."
-# rm -rf priv/lager; pkill -9 beam.smp; RESTART_NODES=false IMPLEMENTATION_MODULE=${MODULE} NUM_TESTS=10 SCHEDULER=finite_fault FAULT_INJECTION=true FAULT_TOLERANCE=1 bin/counterexample-find.sh
+echo "Running multi run fault-injector [with fault-injection, F = 1]..."
+rm -rf priv/lager; pkill -9 beam.smp; RESTART_NODES=false IMPLEMENTATION_MODULE=${MODULE} NUM_TESTS=10 SCHEDULER=finite_fault FAULT_INJECTION=true FAULT_TOLERANCE=1 bin/counterexample-find.sh
 
 # echo "Running multi run fault-injector [with fault-injection, F = 2]..."
 # rm -rf priv/lager; pkill -9 beam.smp; RESTART_NODES=false IMPLEMENTATION_MODULE=${MODULE} NUM_TESTS=10 SCHEDULER=finite_fault FAULT_INJECTION=true FAULT_TOLERANCE=2 bin/counterexample-find.sh
