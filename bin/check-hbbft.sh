de7f868fa075b5bd1760fd5109eb9f296cd5421f
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2019-05-21 06:10:00 -0700

    Add alternative fault model.

--- bin/check-hbbft.sh
+++ bin/check-hbbft.sh
@@ -9,6 +9,8 @@ export PRUNING=false
 export SYSTEM_MODEL=prop_partisan_hbbft
 export PARTISAN=true
 export NUM_TESTS=10
+# export FAULT_MODEL=prop_partisan_arbitrary_fault_model
+export FAULT_MODEL=prop_partisan_crash_fault_model
 
 # echo "Running example suite to identify minimal successful example [with bootstrap]..."
 # rm -rf priv/lager; pkill -9 beam.smp; BOOTSTRAP=true RESTART_NODES=false IMPLEMENTATION_MODULE=${MODULE} NUM_TESTS=3 SCHEDULER=single_success bin/counterexample-find.sh
