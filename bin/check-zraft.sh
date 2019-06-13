cb1111e9fc8e0179b14d301af389dc5f2070be18
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2019-06-13 00:05:33 -0700

    Restore original configuration.

--- bin/check-zraft.sh
+++ bin/check-zraft.sh
@@ -4,9 +4,18 @@ export MODULE=zraft
 export SUBLIST=0
 export PRELOAD_SCHEDULES=false
 export RECURSIVE=true
-export EXIT_ON_COUNTEREXAMPLE=true
+# export EXIT_ON_COUNTEREXAMPLE=true
 export PRUNING=false
 export SYSTEM_MODEL=prop_partisan_zraft
+
+# echo "Running example suite to identify minimal successful example..."
+# rm -rf priv/lager; pkill -9 beam.smp; DISABLE_RANDOM=true RESTART_NODES=false IMPLEMENTATION_MODULE=${MODULE} NUM_TESTS=3 SCHEDULER=single_success bin/counterexample-find.sh
+
+# echo "Replaying existing example..."
+# rm -rf priv/lager; pkill -9 beam.smp; REPLAY=true DISABLE_RANDOM=true RESTART_NODES=false IMPLEMENTATION_MODULE=${MODULE} NUM_TESTS=3 SCHEDULER=single_success bin/counterexample-replay.sh
+
+# echo "Running single run model checker..."
+# rm -rf priv/lager; pkill -9 beam.smp; TRACE_FILE=/tmp/partisan-latest.trace REPLAY_TRACE_FILE=/tmp/partisan-replay.trace PRELOAD_OMISSIONS_FILE=/tmp/partisan-preload.trace SHRINKING=true REPLAY=true SYSTEM_MODEL=${SYSTEM_MODEL} IMPLEMENTATION_MODULE=${MODULE} RESTART_NODES=false ./rebar3 ct --readable=false -v --suite=filibuster_SUITE --case=model_checker_test
 
 echo "Running multi run fault-injector..."
 rm -rf priv/lager; pkill -9 beam.smp; DISABLE_RANDOM=true RESTART_NODES=false IMPLEMENTATION_MODULE=${MODULE} NUM_TESTS=10 SCHEDULER=finite_fault FAULT_INJECTION=true bin/counterexample-find.sh
