8a21856cc536bb2bcec5f28ce238b62af68a2c6b
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2019-09-16 17:21:36 -0400

    Update build configuration.
    - Rename environment variable so that it is no longer just MODULE.
    - Ensure we verify that 2PC keeps working through Travis.

--- bin/check-hbbft.sh
+++ bin/check-hbbft.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export MODULE=hbbft
+export IMPLEMENTATION_MODULE=hbbft
 export SUBLIST=0
 export PRELOAD_SCHEDULES=false
 export RECURSIVE=true
@@ -13,16 +13,16 @@ export NUM_TESTS=10
 export FAULT_MODEL=prop_partisan_crash_fault_model
 
 echo "Running example suite to identify minimal successful example [with bootstrap]..."
-rm -rf priv/lager; pkill -9 beam.smp; BOOTSTRAP=true RESTART_NODES=false IMPLEMENTATION_MODULE=${MODULE} NUM_TESTS=3 SCHEDULER=single_success bin/counterexample-find.sh
+rm -rf priv/lager; pkill -9 beam.smp; BOOTSTRAP=true RESTART_NODES=false IMPLEMENTATION_MODULE=${IMPLEMENTATION_MODULE} NUM_TESTS=3 SCHEDULER=single_success bin/counterexample-find.sh
 
 # echo "Running example suite to identify minimal successful example [without bootstrap]..."
-# rm -rf priv/lager; pkill -9 beam.smp; RESTART_NODES=false IMPLEMENTATION_MODULE=${MODULE} NUM_TESTS=3 SCHEDULER=single_success bin/counterexample-find.sh
+# rm -rf priv/lager; pkill -9 beam.smp; RESTART_NODES=false IMPLEMENTATION_MODULE=${IMPLEMENTATION_MODULE} NUM_TESTS=3 SCHEDULER=single_success bin/counterexample-find.sh
 
 # echo "Running multi run fault-injector [without fault-injection]..."
-# rm -rf priv/lager; pkill -9 beam.smp; RESTART_NODES=false IMPLEMENTATION_MODULE=${MODULE} NUM_TESTS=${NUM_TESTS} SCHEDULER=finite_fault bin/counterexample-find.sh
+# rm -rf priv/lager; pkill -9 beam.smp; RESTART_NODES=false IMPLEMENTATION_MODULE=${IMPLEMENTATION_MODULE} NUM_TESTS=${NUM_TESTS} SCHEDULER=finite_fault bin/counterexample-find.sh
 
 # echo "Running multi run fault-injector [with fault-injection, F = 1]..."
-# rm -rf priv/lager; pkill -9 beam.smp; BOOTSTRAP=true RESTART_NODES=false IMPLEMENTATION_MODULE=${MODULE} NUM_TESTS=${NUM_TESTS} SCHEDULER=finite_fault FAULT_INJECTION=true FAULT_TOLERANCE=1 bin/counterexample-find.sh
+# rm -rf priv/lager; pkill -9 beam.smp; BOOTSTRAP=true RESTART_NODES=false IMPLEMENTATION_MODULE=${IMPLEMENTATION_MODULE} NUM_TESTS=${NUM_TESTS} SCHEDULER=finite_fault FAULT_INJECTION=true FAULT_TOLERANCE=1 bin/counterexample-find.sh
 
 # echo "Running multi run fault-injector [with fault-injection, F = 2]..."
-# rm -rf priv/lager; pkill -9 beam.smp; RESTART_NODES=false IMPLEMENTATION_MODULE=${MODULE} NUM_TESTS=${NUM_TESTS} SCHEDULER=finite_fault FAULT_INJECTION=true FAULT_TOLERANCE=2 bin/counterexample-find.sh
+# rm -rf priv/lager; pkill -9 beam.smp; RESTART_NODES=false IMPLEMENTATION_MODULE=${IMPLEMENTATION_MODULE} NUM_TESTS=${NUM_TESTS} SCHEDULER=finite_fault FAULT_INJECTION=true FAULT_TOLERANCE=2 bin/counterexample-find.sh
--- bin/check-zraft.sh
+++ bin/check-zraft.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export MODULE=zraft
+export IMPLEMENTATION_MODULE=zraft
 export SUBLIST=0
 export PRELOAD_SCHEDULES=false
 export RECURSIVE=true
@@ -10,13 +10,13 @@ export SYSTEM_MODEL=prop_partisan_zraft
 export RESTART_NODES=true 
 
 # echo "Running example suite to identify minimal successful example..."
-# rm -rf priv/lager; pkill -9 beam.smp; DISABLE_RANDOM=true RESTART_NODES=${RESTART_NODES} IMPLEMENTATION_MODULE=${MODULE} NUM_TESTS=3 SCHEDULER=single_success bin/counterexample-find.sh
+# rm -rf priv/lager; pkill -9 beam.smp; DISABLE_RANDOM=true RESTART_NODES=${RESTART_NODES} IMPLEMENTATION_MODULE=${IMPLEMENTATION_MODULE} NUM_TESTS=3 SCHEDULER=single_success bin/counterexample-find.sh
 
 # echo "Replaying existing example..."
-# rm -rf priv/lager; pkill -9 beam.smp; REPLAY=true DISABLE_RANDOM=true RESTART_NODES=${RESTART_NODES} IMPLEMENTATION_MODULE=${MODULE} NUM_TESTS=3 SCHEDULER=single_success bin/counterexample-replay.sh
+# rm -rf priv/lager; pkill -9 beam.smp; REPLAY=true DISABLE_RANDOM=true RESTART_NODES=${RESTART_NODES} IMPLEMENTATION_MODULE=${IMPLEMENTATION_MODULE} NUM_TESTS=3 SCHEDULER=single_success bin/counterexample-replay.sh
 
 # echo "Running single run model checker..."
-# rm -rf priv/lager; pkill -9 beam.smp; TRACE_FILE=/tmp/partisan-latest.trace REPLAY_TRACE_FILE=/tmp/partisan-replay.trace PRELOAD_OMISSIONS_FILE=/tmp/partisan-preload.trace SHRINKING=true REPLAY=true SYSTEM_MODEL=${SYSTEM_MODEL} IMPLEMENTATION_MODULE=${MODULE} RESTART_NODES=${RESTART_NODES} ./rebar3 ct --readable=false -v --suite=filibuster_SUITE --case=model_checker_test
+# rm -rf priv/lager; pkill -9 beam.smp; TRACE_FILE=/tmp/partisan-latest.trace REPLAY_TRACE_FILE=/tmp/partisan-replay.trace PRELOAD_OMISSIONS_FILE=/tmp/partisan-preload.trace SHRINKING=true REPLAY=true SYSTEM_MODEL=${SYSTEM_MODEL} IMPLEMENTATION_MODULE=${IMPLEMENTATION_MODULE} RESTART_NODES=${RESTART_NODES} ./rebar3 ct --readable=false -v --suite=filibuster_SUITE --case=model_checker_test
 
 echo "Running multi run fault-injector..."
-rm -rf priv/lager; pkill -9 beam.smp; DISABLE_RANDOM=true RESTART_NODES=${RESTART_NODES} IMPLEMENTATION_MODULE=${MODULE} NUM_TESTS=10 SCHEDULER=finite_fault FAULT_INJECTION=true bin/counterexample-find.sh
+rm -rf priv/lager; pkill -9 beam.smp; DISABLE_RANDOM=true RESTART_NODES=${RESTART_NODES} IMPLEMENTATION_MODULE=${IMPLEMENTATION_MODULE} NUM_TESTS=10 SCHEDULER=finite_fault FAULT_INJECTION=true bin/counterexample-find.sh
