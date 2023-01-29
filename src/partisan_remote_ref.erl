93a8c9857eb5709a2d20318a1ffe4ecc0a68c7c2
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2023-01-29 22:05:16 +0000

    added todo
    Signed-off-by: Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>

--- src/partisan_remote_ref.erl
+++ src/partisan_remote_ref.erl
@@ -80,6 +80,11 @@
 %%    {partisan_process_reference,"<0.1062.0>"}}
 %% '''
 %%
+%% == Issues and TODOs ==
+%% As opposed to erlang pid encodintg (`NEW_PID_EXT`) our current
+%% representation cannot distinguished between identifiers from old (crashed)
+%% nodes from a new one. So maybe we need to adopt the `NEW_PID_EXT`'s
+%% `Creation` attribute.
 %% @end
 %% -----------------------------------------------------------------------------
 -module(partisan_remote_ref).
