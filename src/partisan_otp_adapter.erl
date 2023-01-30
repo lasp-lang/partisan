15fe56f61aa809b07734647a190731f1666fd0e5
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2023-01-30 21:51:48 +0000

    added todo; overloaded function to accept opts | timeout
    Signed-off-by: Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>

--- src/partisan_otp_adapter.erl
+++ src/partisan_otp_adapter.erl
@@ -19,7 +19,8 @@
 %% -------------------------------------------------------------------
 
 %% -----------------------------------------------------------------------------
-%% @doc THIS MODULE IS DEPRECATED
+%% @doc TODO validate this module, align vs partisan_gen and if useful add
+%% gen_statem API, deprecating fsm API.
 %% @end
 %% -----------------------------------------------------------------------------
 -module(partisan_otp_adapter).
@@ -28,19 +29,26 @@
 
 -include("partisan.hrl").
 
--export([
-         gen_server_cast/3,
-         gen_server_call/4,
-         gen_fsm_send_event/3,
-         gen_fsm_send_all_state_event/3,
-         gen_fsm_sync_send_event/4,
-         gen_fsm_sync_send_all_state_event/4
-        ]).
 
-%%%===================================================================
-%%% gen_server API
-%%%===================================================================
+-export([gen_server_cast/3]).
+-export([gen_server_call/4]).
+-export([gen_fsm_send_event/3]).
+-export([gen_fsm_send_all_state_event/3]).
+-export([gen_fsm_sync_send_event/4]).
+-export([gen_fsm_sync_send_all_state_event/4]).
 
+
+
+%% =============================================================================
+%% GEN_SERVER API
+%% =============================================================================
+
+
+
+%% -----------------------------------------------------------------------------
+%% @doc
+%% @end
+%% -----------------------------------------------------------------------------
 gen_server_cast(Node, Dest, Request) ->
     partisan:forward_message(
         Node,
@@ -49,13 +57,35 @@ gen_server_cast(Node, Dest, Request) ->
         #{channel => ?DEFAULT_CHANNEL}
     ).
 
+
+%% -----------------------------------------------------------------------------
+%% @doc
+%% @end
+%% -----------------------------------------------------------------------------
+gen_server_call(Node, Dest, Request, Opts) when is_list(Opts) ->
+    call(
+        gen_server_call, Node, Dest, '$gen_call', Request, Opts
+    );
+
 gen_server_call(Node, Dest, Request, Timeout) ->
-    call(gen_server_call, Node, Dest, '$gen_call', Request, Timeout).
+    call(
+        gen_server_call, Node, Dest, '$gen_call', Request, [{timeout, Timeout}]
+    ).
 
-%%%===================================================================
-%%% gen_fsm API
-%%%===================================================================
 
+
+
+
+%% =============================================================================
+%% GEN_FSM_API (DEPRECATED)
+%% =============================================================================
+
+
+
+%% -----------------------------------------------------------------------------
+%% @doc Deprecated
+%% @end
+%% -----------------------------------------------------------------------------
 gen_fsm_send_event(Node, Dest, Request) ->
     partisan:forward_message(
         Node,
@@ -73,33 +103,51 @@ gen_fsm_send_all_state_event(Node, Dest, Request) ->
     ).
 
 gen_fsm_sync_send_event(Node, Dest, Request, Timeout) ->
-    call(gen_fsm_sync_send_event, Node, Dest, '$gen_sync_event', Request, Timeout).
+    call(
+        gen_fsm_sync_send_event, Node, Dest, '$gen_sync_event', Request, Timeout
+    ).
 
 gen_fsm_sync_send_all_state_event(Node, Dest, Request, Timeout) ->
-    call(gen_fsm_sync_send_all_state_event, Node, Dest, '$gen_sync_all_state_event', Request, Timeout).
+    call(
+        gen_fsm_sync_send_all_state_event, Node, Dest,
+        '$gen_sync_all_state_event', Request, Timeout
+    ).
 
-%%%===================================================================
-%%% Internal functions
-%%%===================================================================
 
-call(Type, Node, Dest, Label, Request, Timeout) ->
+
+%% =============================================================================
+%% PRIVATE
+%% =============================================================================
+
+
+
+call(Type, Node, Dest, Label, Request, Opts0) ->
     %% Make reference.
     Mref = make_ref(),
 
     %% Get our pid.
     Self = partisan:self(),
 
+    {Timeout, Opts} =
+        case lists:keytake(timeout, 1, Opts0) of
+            {value, {timeout, Val}, Opts1} ->
+                {Val, Opts1};
+            false ->
+                {5000, Opts0}
+        end,
+
     %% Send message.
     partisan:forward_message(
         Node,
         Dest,
         {Label, {Self, Mref}, Request},
-        #{channel => ?DEFAULT_CHANNEL}
+        Opts
     ),
 
-    %% Don't timeout earlier than the timeout -- Distributed Erlang would if the net_ticktime fired and
-    %% determined that the node is down.  However, this adds nondeterminism into the execution, so wait until
-    %% the timeout.  This is still nondeterministic, but less so.
+    %% Don't timeout earlier than the timeout -- Distributed Erlang would if
+    %% the net_ticktime fired and determined that the node is down.  However,
+    %% this adds nondeterminism into the execution, so wait until
+    %% the timeout. This is still nondeterministic, but less so.
     receive
         {Mref, Reply} ->
             Reply
